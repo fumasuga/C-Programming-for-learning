bd53f9f3eb83fbb5e40575912d267866ba96322f
Fuma Suga <f0976917@gmail.com>
2025-03-30 17:22:28 +0900

    comments, escape sequence

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main() {
-
+    // This is a simple C program that prints "Hello, World!" to the console.
+    /*
+        This is a multi-line comment.
+    */
     printf("Hello, World!\n");
-    printf("This is a simple C program.\n");
+    printf("\"I am learning C programming!\"\t 2025\n");
     return 0;
 }
