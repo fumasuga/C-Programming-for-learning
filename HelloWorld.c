6e41c209d1499e3df99dfe69d04cc77b6a9dc3d6
Fuma Suga <f0976917@gmail.com>
2025-03-30 17:31:01 +0900

    variable

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    // This is a simple C program that prints "Hello, World!" to the console.
-    /*
-        This is a multi-line comment.
-    */
-    printf("Hello, World!\n");
-    printf("\"I am learning C programming!\"\t 2025\n");
+    
+    int x; //declaration
+    x = 5; //initialization
+    int y = 10; //declaration and initialization
+
+    int age = 25; //integer
+    float gpa = 2.8; //float
+    char grade = 'A'; //single character
+    char name[] = "John Doe"; //array of characters (string)
+
+    printf("Hello, %s\n", name); //%s is a format specifier for strings
+    printf("You are %d years old\n", age); //%d is a format specifier for integers
+    printf("Your GPA is %.2f\n", gpa); //%.2f is a format specifier for float with 2 decimal places
+    printf("Your grade is %c\n", grade); //%c is a format specifier for characters
     return 0;
 }
