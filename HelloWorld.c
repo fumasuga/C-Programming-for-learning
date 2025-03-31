c16cd38bc3c2a9c7cb8eeb03aa336d56bb068c2a
Fuma Suga <f0976917@gmail.com>
2025-03-31 23:19:41 +0900

    constants

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    
-    float item1 = 3.14;
-    float item2 = 31.4;
-    float item3 = 314.15;
 
-    printf("Item 1: %8.2f\n", item1); // 8 spaces wide, 2 decimal places
-    printf("Item 2: %2f\n", item2); // default width, default decimal places
-    printf("Item 3: %-3.2f\n", item3); // 3 spaces wide, 2 decimal places, left-aligned
+    const float pi = 3.14159;
+
+    printf("%f\n", pi);
     return 0;
 }
