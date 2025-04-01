7e72a229619846c14d0fef6d03dee13234c08e87
Fuma Suga <f0976917@gmail.com>
2025-04-01 23:30:26 +0900

    if statements

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 
-    double a;
-    double b;
-    double c;
+    int age;
 
-    printf("Enter the first number: ");
-    scanf("%lf", &a);
+    printf("\nHow old are you?");
+    scanf("%d", &age);
 
-    printf("Enter the second number: ");
-    scanf("%lf", &b);
+    if(age >= 20){
+        printf("You are an adult.\n");
+    } else if(age >= 13 && age < 20){
+        printf("You are a teenager.\n");
+    } else if(age >= 0 && age < 13){
+        printf("You are a child.\n");
+    } else {
+        printf("Invalid age.\n");
+    }
 
-    c = sqrt(a * a + b * b);
-    printf("The length of the hypotenuse is: %.2f\n", c);
     return 0;
 }
