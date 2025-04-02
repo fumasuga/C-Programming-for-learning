661f35af15a1594e09282d6185ce088d9be34636
Fuma Suga <f0976917@gmail.com>
2025-04-02 22:33:11 +0900

    functions

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,44 +1,18 @@
 #include <stdio.h>
 
+// This program prints "Hello, World!" and then greets three other people.
+void helloWorld() {
+    printf("Hello, World!\n");
+    printf("Hello, John!\n");
+    printf("Hello, Jane!\n");
+    printf("Hello, Alice!\n");
+}
+
 int main() {
 
-    char operator;
-    double num1, num2, result;
-
-    printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &operator);
-
-    printf("Enter number 1: ");
-    scanf("%lf", &num1);
-
-    printf("Enter number 2: ");
-    scanf("%lf", &num2);
-
-    // Perform the operation based on the operator entered
-    switch (operator) {
-        case '+':
-            result = num1 + num2;
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
-            break;
-        case '-':
-            result = num1 - num2;
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
-            break;
-        case '*':
-            result = num1 * num2;
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
-            } else {
-                printf("Error! Division by zero.\n");
-            }
-            break;
-        default:
-            printf("Error! Invalid operator.\n");
-    }
+    helloWorld();
+    helloWorld();
+    helloWorld();
 
     return 0;
 }
