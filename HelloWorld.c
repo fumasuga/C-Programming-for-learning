6b1c7cc288ce75933a86ff4e63727cb01197b5e6
Fuma Suga <f0976917@gmail.com>
2025-04-03 22:35:59 +0900

    return statement

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 
-// This program asks the user for their name and age, then prints a greeting message.
-void hello(char name[], int age) {
-    printf("Hello, %s!\n", name);
-    printf("You are %d years old.\n", age);
+// Function to calculate the sum of divisors of a number
+int divisor(int n) {
+    int i, total = 0;
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            total += i;
+        }
+    }
+    return total;
 }
 
 int main() {
 
-    char name[20];
-    int age;
-
-    printf("what is your name? ");
-    scanf("%s", name);
-    printf("How old are you? ");
-    scanf("%d", &age);
-
-    hello(name, age);
+    int n, result, divisorSum;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    divisorSum = divisor(n);
+    if (divisorSum == n*2) {
+        printf("%d is a perfect number\n", n);
+    } else if (divisorSum < n) {
+        printf("%d is a deficient number\n", n);
+    } else {
+        printf("%d is an abundant number\n", n);
+    }
     return 0;
 }
