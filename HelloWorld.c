14d912bd29f9a59e884544efcaeab6598b9d5e80
Fuma Suga <f0976917@gmail.com>
2025-04-04 22:06:29 +0900

    function prototypes

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 
-// Function to calculate the sum of divisors of a number
-int divisor(int n) {
-    int i, total = 0;
-    for (i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            total += i;
-        }
-    }
-    return total;
-}
+void hello(char[], int); // Function prototype
+// Function prototype is a declaration of a function that specifies the function's name, return type, and parameters.
+// It allows the compiler to check for correct usage of the function before its actual definition.
+// it is typically placed before the main function or in a header file.
 
 int main() {
-
-    int n, result, divisorSum;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    divisorSum = divisor(n);
-    if (divisorSum == n*2) {
-        printf("%d is a perfect number\n", n);
-    } else if (divisorSum < n) {
-        printf("%d is a deficient number\n", n);
-    } else {
-        printf("%d is an abundant number\n", n);
-    }
+    
+    char name[50];
+    int age;
+    
+    printf("Enter your name: ");
+    scanf("%s", name);
+    
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    
+    hello(name, age);
     return 0;
+}
+
+void hello(char name[], int age) {
+    printf("Hello, %s! You are %d years old.\n", name, age);
 }
