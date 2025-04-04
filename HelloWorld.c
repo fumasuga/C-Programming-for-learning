7781d00bca3f48c1ae64da5d6870ca167f341cde
Fuma Suga <f0976917@gmail.com>
2025-04-04 22:30:06 +0900

    while loops

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
-    // for loop is used to iterate over a range of values
-    for (int i = 1; i <= 5; i++) {
-        printf("%d\n", i);
+    char name[50];
+
+    printf("what is your name? ");
+    fgets(name, sizeof(name), stdin); // Read a line of input from the user
+
+    name[strlen(name -1)] = '\0';
+    
+    while(strlen(name) == 0) {
+        printf("Please enter a valid name: ");
+        fgets(name, sizeof(name), stdin); // Read a line of input from the user
+        name[strlen(name -1)] = '\0';
     }
+
+    printf("Hello, %s\n", name);
     return 0;
 }
