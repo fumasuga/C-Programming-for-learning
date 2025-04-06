786f8c654dfea39a7b614355e73b036a674b58b8
Fuma Suga <f0976917@gmail.com>
2025-04-06 22:09:32 +0900

    print an array with loop

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,16 +2,18 @@
 
 int main() {
 
-    // array is a collection of data items of the same type
-    double scores[] = {85.5, 90.0, 78.5, 92.0, 88.0};
-    double average = 0.0;
-    
-    printf("english score is %.2f\n", scores[0]);
-    printf("math score is %.2f\n", scores[1]);
-    printf("science score is %.2f\n", scores[2]);
-    printf("history score is %.2f\n", scores[3]);
-    printf("art score is %.2f\n", scores[4]);
-    average = (scores[0] + scores[1] + scores[2] + scores[3] + scores[4]) / 5.0;
-    printf("average score is %.2f\n", average);
+    printf("Enter amount of numbers to be entered: ");
+    int n;
+    scanf("%d", &n);
+    double numbers[n];
+    printf("Enter %d numbers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%lf", &numbers[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("Number %d: %.2f\n", i + 1, numbers[i]);
+    }
+
     return 0;
 }
