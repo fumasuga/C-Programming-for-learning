8b38329f74e222dc327b2065f3a09bc31df87504
Fuma Suga <f0976917@gmail.com>
2025-04-06 22:01:22 +0900

    arrays

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,18 +2,16 @@
 
 int main() {
 
-    int x = 0;
-    printf("please enter a number: ");
-    scanf("%d", &x);
-    for (int i = 0; i <= x; i++) {
-        if (i == 19) {
-            continue; // Skip the number 19
-        }
-        if (i == 25) {
-            break; // Stop the loop when i is 25
-        }
-        printf("%d\n", i);
-
-    }
+    // array is a collection of data items of the same type
+    double scores[] = {85.5, 90.0, 78.5, 92.0, 88.0};
+    double average = 0.0;
+    
+    printf("english score is %.2f\n", scores[0]);
+    printf("math score is %.2f\n", scores[1]);
+    printf("science score is %.2f\n", scores[2]);
+    printf("history score is %.2f\n", scores[3]);
+    printf("art score is %.2f\n", scores[4]);
+    average = (scores[0] + scores[1] + scores[2] + scores[3] + scores[4]) / 5.0;
+    printf("average score is %.2f\n", average);
     return 0;
 }
