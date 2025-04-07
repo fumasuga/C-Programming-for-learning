42678e373735aa8ff288878e64fa3f309fcb2243
Fuma Suga <f0976917@gmail.com>
2025-04-07 23:09:26 +0900

    array of strings

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,30 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
-    printf("Enter number of rows: ");
+    char car[][20] = {"BMW", "Mercedes", "Audi", "Porsche"};
+    // cars[0] = "Toyota";
+    strcpy(car[0], "Toyota"); // Copying string into the first element of the array
 
-    int rows;
-    scanf("%d", &rows);
-
-    printf("Enter number of columns: ");
-    int columns;
-    scanf("%d", &columns);
-
-    int matrix[rows][columns];
-    printf("Enter the elements of the matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-
-    printf("The matrix is:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
+    for(int i = 0; i < sizeof(car)/sizeof(car[0]); i++) {
+        printf("%s\n", car[i]);
     }
 
     return 0;
