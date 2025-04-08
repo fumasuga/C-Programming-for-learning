4b3912a07a600af734fa09d4d0bc0e50c9cb6b3c
Fuma Suga <f0976917@gmail.com>
2025-04-08 23:25:53 +0900

    random numbers

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
-
-enum Day {Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4, Thursday = 5, Friday = 6, Saturday = 7};
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
 
-    // enum is a user-defined type that consists of a set of named integer constants.
-    enum Day today = Wednesday;
-
-    printf("Today is day number %d of the week.\n", today);
-
-    if (today == Wednesday) {
-        printf("It's the middle of the week!\n");
-    } else if (today == Monday) {
-        printf("It's the start of the week!\n");
-    } else if (today == Sunday || today == Saturday) {\
-        printf("It's a weekday.\n");
-    } else {
-        printf("It's a regular weekday.\n");
-    }
+    srand(time(NULL)); // Seed the random number generator
+    int random_number1 = rand() % 100; // Generate a random number between 0 and 99
+    int random_number2 = rand() % 100;
+    int random_number3 = rand() % 100;
+    printf("Random number: %d\n", random_number1);
+    printf("Random number: %d\n", random_number2);
+    printf("Random number: %d\n", random_number3);
     return 0;
 }
