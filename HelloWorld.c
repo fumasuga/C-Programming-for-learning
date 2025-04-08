99879a4752b7a9151afc48bc5d89e28933ce8091
Fuma Suga <f0976917@gmail.com>
2025-04-08 23:14:54 +0900

    array of structs

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-// typedef is used to create a new name for an existing type
-// typedef char user[25]; // Define a new type 'user' as an array of 25 characters
-typedef struct {
-    char name[25]; // Define a new type 'user' as a structure with a character array of size 25
-    int age;
-} User;
+struct Student {
+    char name[50];
+    float gpa;
+};
 
 int main() {
 
-    // user user1 = "Num";
+    struct Student student1 = {"Mike", 2.5};
+    struct Student student2 = {"John", 3.8};
+    struct Student student3 = {"Alice", 2.9};
+    struct Student student4 = {"Bob", 2.2};
+    struct Student student5 = {"Eve", 1.7};
+    
+    struct Student students[] = {student1, student2, student3, student4, student5};
 
-    User user1 = {"Joji", 25}; // Initialize a user structure with name "Joji" and age 25
-    User user2 = {"Fox", 29}; // Initialize another user structure with name "Fox" and age 29
-
-    printf("Hello %s\n", user1.name); // Print the name of user1
-    printf("Hello %s\n", user2.name); // Print the name of user2
-
+    for(int i = 0; i < sizeof(students)/sizeof(students[0]); i++) {
+        printf("Name: %s, GPA: %.2f\n", students[i].name, students[i].gpa);
+    }
     return 0;
 }
