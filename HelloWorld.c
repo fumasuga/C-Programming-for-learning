6e07ec7e900f32a5b2ed561e847d30719415b790
Fuma Suga <f0976917@gmail.com>
2025-04-11 23:29:26 +0900

    reading files

--- HelloWorld.c
+++ HelloWorld.c
@@ -9,6 +9,17 @@ int main() {
 
     fclose(pF); // Close the file
 
+    FILE *pF2 = fopen("test.txt", "r"); // Open the file in read mode
+    char buffer[100]; // Buffer to hold the read data
+    if (pF2 != NULL) {
+        while (fgets(buffer, sizeof(buffer), pF2) != NULL) { // Read the file line by line
+            printf("%s", buffer); // Print the read data to the console
+        }
+        fclose(pF2); // Close the file
+    } else {
+        printf("Error opening the file for reading.\n");
+    }
+    
     if (remove("test.txt") == 0) { // Remove the file
         printf("File deleted successfully.\n");
     } else {
