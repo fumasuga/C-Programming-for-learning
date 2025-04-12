d5b9ed56058c9c53abc34f5134d8ca89f576d240
Fuma Suga <f0976917@gmail.com>
2025-04-12 23:40:09 +0900

    va list

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,30 +1,15 @@
 #include <stdio.h>
 
+typedef char *va_list; // This is a typedef for the va_list type, which is used for variable argument lists in functions like printf.
+
+typedef struct {
+    unsigned int_offset; // This is an unsigned integer offset.
+    unsigned int fp_offset; // This is another unsigned integer offset.
+    void *overflow_arg_area; // This is a pointer to an overflow argument area.
+    void *reg_save_area; // This is a pointer to a register save area.
+} va_list[1];
+
 int main() {
-
-    FILE *pF = fopen("test.txt", "w"); // Open the file in write mode
-    
-    fprintf(pF, "This is \n"); // Write to the file
-    fprintf(pF, "a test file.\n"); // Write to the file
-
-    fclose(pF); // Close the file
-
-    FILE *pF2 = fopen("test.txt", "r"); // Open the file in read mode
-    char buffer[100]; // Buffer to hold the read data
-    if (pF2 != NULL) {
-        while (fgets(buffer, sizeof(buffer), pF2) != NULL) { // Read the file line by line
-            printf("%s", buffer); // Print the read data to the console
-        }
-        fclose(pF2); // Close the file
-    } else {
-        printf("Error opening the file for reading.\n");
-    }
-    
-    if (remove("test.txt") == 0) { // Remove the file
-        printf("File deleted successfully.\n");
-    } else {
-        printf("Error deleting the file.\n");
-    }
 
     return 0;
 }
